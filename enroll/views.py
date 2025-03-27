357b565cfc82345dee45b40ef5a48b63a474bd8b
divine122 <ogochukwudivine221@gmail.com>
2025-03-27 13:11:44 +0100

    FormIntegration

--- enroll/views.py
+++ enroll/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from.forms import StudentRegistration
+from .models import User
 # Create your views here.
 def add_show(request):
     if request.method == 'POST':
         fm = StudentRegistration(request.POST)
         if fm.is_valid():
-            fm.save()
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+            return redirect("/")
     else:
-        fm = StudentRegistration()    
-    return render(request, 'enroll/addandshow.html', {'form':fm})
+        fm = StudentRegistration()  
+    
+    stud = User.objects.all()  
+    return render(request, 'enroll/addandshow.html', {'form':fm,'stud':stud})
